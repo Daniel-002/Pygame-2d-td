6a6b84b12028c9453290fe0e323fccd40a99d19b
DanielB <danielbivc@gmail.com>
2020-11-06 12:54:39 +0100

    astronaut png

--- marsgame.py
+++ marsgame.py
@@ -3,12 +3,27 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Mars TD')
+
+astronaut = pygame.image.load('astronaut/astronaut-frame1.png')
+background = pygame.image.load('pixil-layer-Background.png')
 done = False
 is_blue = True
 x = 30
 y = 30
 
 clock = pygame.time.Clock()
+
+def drawingscreen():
+    screen.blit(background, (0,0))
+    if is_blue:
+        color = (255, 0, 0)
+    else:
+        color = (255, 100, 0)
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+
+    pygame.display.flip()
+    clock.tick(60)
 
 while not done:
     for event in pygame.event.get():
@@ -23,12 +38,5 @@ while not done:
     if pressed[pygame.K_LEFT]: x -= 3
     if pressed[pygame.K_RIGHT]: x += 3
 
-    screen.fill((0, 0, 0))
-    if is_blue:
-        color = (255, 0, 0)
-    else:
-        color = (255, 100, 0)
-    pygame.draw.rect(screen, color, pygame.Rect(x, y, 60, 60))
+    drawingscreen()
 
-    pygame.display.flip()
-    clock.tick(60)
